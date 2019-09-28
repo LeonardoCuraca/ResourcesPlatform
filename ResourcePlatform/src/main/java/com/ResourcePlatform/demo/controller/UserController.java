a69cea570a8b8197012e98968b61abd7123b77b6
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2019-09-28 06:43:30 -0500

    login, registro y principal

--- ResourcePlatform/src/main/java/com/ResourcePlatform/demo/controller/UserController.java
+++ ResourcePlatform/src/main/java/com/ResourcePlatform/demo/controller/UserController.java
@@ -1,14 +1,24 @@
 package com.ResourcePlatform.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bin.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 	
 	@GetMapping("/")
 	public String index() {
-		return "index";
+		return "login";
+	}
+	
+	@GetMapping("/main")
+	public String main() {
+		return "main";
+	}
+	
+	@GetMapping("/register")
+	public String register() {
+		return "register";
 	}
 	
 }
